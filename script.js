684b79429961e0f30c8158125561db89db828235
unknown <jfcchisco@gmail.com>
2026-01-17 21:30:05 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,197 @@
+gsap.registerPlugin(Draggable, InertiaPlugin);
+
+let $view  = document.querySelector("#view");
+let $image = document.querySelector("#image");
+let $reset = document.querySelector("#reset");
+
+let firstRun = true;
+
+let viewWidth  = window.innerWidth;
+let viewHeight = window.innerHeight;
+
+let imageWidth  = $image.width;
+let imageHeight = $image.height;
+
+let zoom = {  
+    accel: 0.1, 
+    chaseScale: 1,
+    chaseX: 0,
+    chaseY: 0,  
+    min: 0.0001,
+    max: 10,
+    scale: 1,
+    scaleFactor: 1.1
+};
+
+let pointer = {
+    x: viewWidth  / 2,
+    y: viewHeight / 2
+};
+
+let setX = gsap.quickSetter($image, "x", "px");
+let setY = gsap.quickSetter($image, "y", "px");
+let setScaleX = gsap.quickSetter($image, "scaleX");
+let setScaleY = gsap.quickSetter($image, "scaleY");
+
+gsap.set($image, { force3D: true, transformOrigin: "left top" });
+
+let tracker = InertiaPlugin.track($image, "x,y")[0];
+let props = gsap.getProperty($image);
+
+let throwTween = gsap.to({}, {});
+let resetTween = gsap.to({}, {});
+
+let draggable = new Draggable($image, {
+    trigger: $view,
+    onDrag: onDrag,
+    onPress: onPress,
+    onRelease: onRelease
+});
+
+init();
+
+function init() {
+    
+    resetView();  
+    
+    $reset.addEventListener("click", resetView);
+    $view.addEventListener("mousewheel", mouseWheel);
+    $view.addEventListener("DOMMouseScroll", mouseWheel);  
+    window.addEventListener("resize", resize);
+    gsap.ticker.add(updateZoom);
+    gsap.set($view, { autoAlpha: 1 });
+    
+    firstRun = false;
+}
+
+function mouseWheel(event) {
+    
+    event.preventDefault();
+    
+    throwTween.kill();
+    
+    pointer.x = event.clientX;
+    pointer.y = event.clientY;
+    
+    let wheel = event.detail || event.deltaY || 0;
+    
+    if (wheel > 0) {
+        zoom.chaseScale /= zoom.scaleFactor;
+    } else {
+        zoom.chaseScale *= zoom.scaleFactor;
+    }
+    
+    zoom.chaseScale = gsap.utils.clamp(zoom.min, zoom.max, zoom.chaseScale);
+}
+
+function updateZoom() {
+  
+    if (resetTween.isActive()) {
+        return;
+    }
+  
+    let oldZoom = zoom.scale;
+    
+    zoom.scale += (zoom.chaseScale - zoom.scale) * zoom.accel;
+    
+    let zoomDelta = zoom.scale - oldZoom;
+        
+    let scale = props("scaleX");  
+    let x = props("x");
+    let y = props("y");
+    
+    let localX = (pointer.x - x) / scale;
+    let localY = (pointer.y - y) / scale;
+    
+    x += -(localX * zoomDelta);
+    y += -(localY * zoomDelta);
+        
+    x = gsap.utils.clamp(-(imageWidth  * zoom.scale), viewWidth, x);
+    y = gsap.utils.clamp(-(imageHeight * zoom.scale), viewHeight, y);
+    
+    setScaleX(zoom.scale);
+    setScaleY(zoom.scale);
+    setX(x);
+    setY(y);
+    
+    // gsap.set($image, {
+    //   scale: zoom.scale,
+    //   x: x,
+    //   y: y
+    // });
+}
+
+function onDrag() {
+    
+    zoom.chaseX = this.x;
+    zoom.chaseY = this.y;
+}
+
+function onPress() {
+    
+    throwTween.kill();
+    
+    zoom.chaseX = this.x;
+    zoom.chaseY = this.y;
+    
+    this.update();
+}
+
+function onRelease() {
+    
+    throwTween = gsap.to(zoom, {      
+        inertia: {
+            resistance: 2000,
+            chaseX: {
+            velocity: tracker.get("x"),
+            min: -(imageWidth  * zoom.scale) - 100,
+            max: viewWidth + 100
+            },
+            chaseY: {
+            velocity: tracker.get("y"),
+            min: -(imageHeight  * zoom.scale) - 100,
+            max: viewHeight + 100
+            }
+        },
+        onUpdate: function() {
+            
+            setX(zoom.chaseX);
+            setY(zoom.chaseY);
+            // gsap.set($image, {
+            //   x: zoom.chaseX,
+            //   y: zoom.chaseY
+            // });
+        }
+    });
+}
+
+function resetView() {
+  
+    throwTween.kill();
+    
+    let sx = viewWidth  / imageWidth;
+    let sy = viewHeight / imageHeight;
+    
+    let scale = Math.min(sx, sy);
+    
+    let x = viewWidth  / 2 - imageWidth  / 2 * scale;
+    let y = viewHeight / 2 - imageHeight / 2 * scale;
+    
+    zoom.chaseScale = scale;
+    zoom.chaseX = x;
+    zoom.chaseY = y;
+    zoom.scale = scale;
+    
+    resetTween = gsap.to($image, {
+        duration: firstRun ? 0 : 0.5,
+        ease: "expo.out",
+        scale: scale,
+        x: x,
+        y: y
+    });  
+}
+
+function resize() {
+    viewWidth  = window.innerWidth;
+    viewHeight = window.innerHeight;
+}
